a9dc36273ffad59a638e45488d3a7152ba41d10c
Aakash Singh <aakkashsingh01@gmail.com>
2025-09-04 00:23:52 +0530

    first commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,138 @@
+// const { User } = require("../models");
+// const {
+//   generateToken,
+//   generateHashedPassword,
+//   verifyPassword,
+// } = require("../config");
+
+// // @description     Register new user
+// // @route           POST /api/user/
+// // @access          Public
+// const registerUser = async (req, res) => {
+//   const { name, email, password, pic } = req.body;
+
+//   // Check if any of them is undefined
+//   if (!name || !email || !password) {
+//     return res.status(400).json({
+//       success: false,
+//       statusCode: 400,
+//       message: "Please enter all the fields",
+//     });
+//   }
+
+//   // Check if user already exists in our DB
+//   const userExists = await User.findOne({ email }).exec();
+
+//   if (userExists) {
+//     return res.status(400).json({
+//       success: false,
+//       statusCode: 400,
+//       message: "User already exists",
+//     });
+//   }
+
+//   // Register and store the new user
+//   const userCreated = await User.create(
+//     // If there is no picture present, remove 'pic'
+//     pic === undefined || pic.length === 0
+//       ? {
+//           name,
+//           email,
+//           password: await generateHashedPassword(password),
+//         }
+//       : {
+//           name,
+//           email,
+//           password: await generateHashedPassword(password),
+//           pic,
+//         }
+//   );
+
+//   if (userCreated) {
+//     return res.status(201).json({
+//       success: true,
+//       statusCode: 201,
+//       _id: userCreated._id,
+//       name: userCreated.name,
+//       email: userCreated.email,
+//       pic: userCreated.pic,
+//       token: generateToken(userCreated._id, userCreated.email),
+//       message: "User Created Successfully",
+//     });
+//   } else {
+//     return res.status(400).json({
+//       success: false,
+//       statusCode: 400,
+//       message: "Failed to create the User",
+//     });
+//   }
+// };
+
+// // @description     Auth the user
+// // @route           POST /api/users/login
+// // @access          Public
+// const authUser = async (req, res) => {
+//   const { email, password } = req.body;
+
+//   // Check if any of them is undefined
+//   if (!email || !password) {
+//     return res.status(400).json({
+//       success: false,
+//       statusCode: 400,
+//       message: "Please enter all the fields",
+//     });
+//   }
+
+//   // Check if user already exists in our DB
+//   const userExists = await User.findOne({ email }).exec();
+
+//   // If user exists and password is verified
+//   if (userExists && (await verifyPassword(password, userExists.password))) {
+//     return res.status(200).json({
+//       success: true,
+//       statusCode: 200,
+//       _id: userExists._id,
+//       name: userExists.name,
+//       email: userExists.email,
+//       pic: userExists.pic,
+//       token: generateToken(userExists._id, userExists.email),
+//       message: "Authenticated Successfully",
+//     });
+//   } else {
+//     return res.status(400).json({
+//       success: false,
+//       statusCode: 400,
+//       message: "Invalid Email or Password",
+//     });
+//   }
+// };
+
+// // @description     Get or Search all users
+// // @route           GET /api/user?search=
+// // @access          Public
+// const allUsers = async (req, res) => {
+//   // Keyword contains search results
+//   const keyword = req.query.search
+//     ? {
+//         $or: [
+//           { name: { $regex: req.query.search, $options: "i" } },
+//           { email: { $regex: req.query.search, $options: "i" } },
+//         ],
+//       }
+//     : {};
+
+//   // Find and return users except current user
+//   const userExists = await User.find(keyword)
+//     .find({ _id: { $ne: req.user._id } })
+//     .exec();
+
+//   return res.status(200).json(userExists);
+// };
+
+// module.exports = { registerUser, authUser, allUsers };
+
+
+
 const { User } = require("../models");
 const {
   generateToken,
@@ -9,61 +144,71 @@ const {
 // @route           POST /api/user/
 // @access          Public
 const registerUser = async (req, res) => {
-  const { name, email, password, pic } = req.body;
+  try {
+    const { name, email, password, pic } = req.body;
 
-  // Check if any of them is undefined
-  if (!name || !email || !password) {
-    return res.status(400).json({
+    // Check if any of them is undefined
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        message: "Please enter all the fields",
+      });
+    }
+
+    // Check if user already exists in our DB
+    const userExists = await User.findOne({ email }).exec();
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        message: "User already exists",
+      });
+    }
+
+    // Register and store the new user
+    const userCreated = await User.create(
+      // If there is no picture present, remove 'pic'
+      pic === undefined || pic.length === 0
+        ? {
+            name,
+            email,
+            password: await generateHashedPassword(password),
+          }
+        : {
+            name,
+            email,
+            password: await generateHashedPassword(password),
+            pic,
+          }
+    );
+
+    if (userCreated) {
+      return res.status(201).json({
+        success: true,
+        statusCode: 201,
+        _id: userCreated._id,
+        name: userCreated.name,
+        email: userCreated.email,
+        pic: userCreated.pic,
+        token: generateToken(userCreated._id, userCreated.email),
+        message: "User Created Successfully",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        message: "Failed to create the User",
+      });
+    }
+  } catch (error) {
+    console.error("Error in registerUser:", error);
+    return res.status(500).json({
       success: false,
-      statusCode: 400,
-      message: "Please enter all the fields",
-    });
-  }
-
-  // Check if user already exists in our DB
-  const userExists = await User.findOne({ email }).exec();
-
-  if (userExists) {
-    return res.status(400).json({
-      success: false,
-      statusCode: 400,
-      message: "User already exists",
-    });
-  }
-
-  // Register and store the new user
-  const userCreated = await User.create(
-    // If there is no picture present, remove 'pic'
-    pic === undefined || pic.length === 0
-      ? {
-          name,
-          email,
-          password: await generateHashedPassword(password),
-        }
-      : {
-          name,
-          email,
-          password: await generateHashedPassword(password),
-          pic,
-        }
-  );
-
-  if (userCreated) {
-    return res.status(201).json({
-      success: true,
-      statusCode: 201,
-      _id: userCreated._id,
-      name: userCreated.name,
-      email: userCreated.email,
-      pic: userCreated.pic,
-      token: generateToken(userCreated._id, userCreated.email),
-      message: "User Created Successfully",
-    });
-  } else {
-    return res.status(400).json({
-      success: false,
-      statusCode: 400,
-      message: "Failed to create the User",
+      statusCode: 500,
+      message: "Server Error",
+      error: error.message,
     });
   }
 };
@@ -72,37 +217,47 @@ const registerUser = async (req, res) => {
 // @route           POST /api/users/login
 // @access          Public
 const authUser = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // Check if any of them is undefined
-  if (!email || !password) {
-    return res.status(400).json({
+    // Check if any of them is undefined
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        message: "Please enter all the fields",
+      });
+    }
+
+    // Check if user already exists in our DB
+    const userExists = await User.findOne({ email }).exec();
+
+    // If user exists and password is verified
+    if (userExists && (await verifyPassword(password, userExists.password))) {
+      return res.status(200).json({
+        success: true,
+        statusCode: 200,
+        _id: userExists._id,
+        name: userExists.name,
+        email: userExists.email,
+        pic: userExists.pic,
+        token: generateToken(userExists._id, userExists.email),
+        message: "Authenticated Successfully",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        message: "Invalid Email or Password",
+      });
+    }
+  } catch (error) {
+    console.error("Error in authUser:", error);
+    return res.status(500).json({
       success: false,
-      statusCode: 400,
-      message: "Please enter all the fields",
-    });
-  }
-
-  // Check if user already exists in our DB
-  const userExists = await User.findOne({ email }).exec();
-
-  // If user exists and password is verified
-  if (userExists && (await verifyPassword(password, userExists.password))) {
-    return res.status(200).json({
-      success: true,
-      statusCode: 200,
-      _id: userExists._id,
-      name: userExists.name,
-      email: userExists.email,
-      pic: userExists.pic,
-      token: generateToken(userExists._id, userExists.email),
-      message: "Authenticated Successfully",
-    });
-  } else {
-    return res.status(400).json({
-      success: false,
-      statusCode: 400,
-      message: "Invalid Email or Password",
+      statusCode: 500,
+      message: "Server Error",
+      error: error.message,
     });
   }
 };
@@ -111,22 +266,33 @@ const authUser = async (req, res) => {
 // @route           GET /api/user?search=
 // @access          Public
 const allUsers = async (req, res) => {
-  // Keyword contains search results
-  const keyword = req.query.search
-    ? {
-        $or: [
-          { name: { $regex: req.query.search, $options: "i" } },
-          { email: { $regex: req.query.search, $options: "i" } },
-        ],
-      }
-    : {};
+  try {
+    // Keyword contains search results
+    const keyword = req.query.search
+      ? {
+          $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
 
-  // Find and return users except current user
-  const userExists = await User.find(keyword)
-    .find({ _id: { $ne: req.user._id } })
-    .exec();
+    // Find and return users except current user
+    const userExists = await User.find(keyword)
+      .find({ _id: { $ne: req.user._id } })
+      .exec();
 
-  return res.status(200).json(userExists);
+    return res.status(200).json(userExists);
+  } catch (error) {
+    console.error("Error in allUsers:", error);
+    return res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = { registerUser, authUser, allUsers };
+
