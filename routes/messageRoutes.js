fbdb1f185de773c54b81e5e3058d0f7a2aadb515
DARSHAN D <darshankdevaraja@gmail.com>
2025-09-05 16:55:53 +0530

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const {
   sendMessage,
   allMessages,
+  reactToMessage,
 } = require("../controllers/messageControllers");
-
 const { protect } = require("../middleware");
 
 const router = express.Router();
@@ -11,5 +11,7 @@ const router = express.Router();
 router.route("/").post(protect, sendMessage);
 
 router.route("/:chatId").get(protect, allMessages);
+
+router.route("/:messageId/react").post(protect, reactToMessage);
 
 module.exports = router;
